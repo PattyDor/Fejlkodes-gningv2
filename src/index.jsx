09d17465f8b990a172f6bdcd70afe32195fd8f03
PattyDor <pado@ok.dk>
2025-03-06 13:02:04 +0100

    Create index.jsx

---
+++ src/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+
+const root = document.getElementById("root");
+
+if (!root) {
+  document.body.innerHTML = "<h1>Error: Root element not found</h1>";
+} else {
+  ReactDOM.createRoot(root).render(<App />);
+}
