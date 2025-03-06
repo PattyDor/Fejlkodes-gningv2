6b47a4372dfdc96f7f0d865e647d594d53110349
PattyDor <pado@ok.dk>
2025-03-06 13:48:25 +0100

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -2,10 +2,5 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
-const root = document.getElementById("root");
-
-if (!root) {
-  document.body.innerHTML = "<h1>Error: Root element not found</h1>";
-} else {
-  ReactDOM.createRoot(root).render(<App />);
-}
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
