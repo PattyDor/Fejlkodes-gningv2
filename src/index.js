84c4a30e7e907db958e4cd1cb520279ff7b88d29
PattyDor <pado@ok.dk>
2025-03-06 12:20:34 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import App from "./App.jsx"; // <-- Ensure this matches the new file name
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(React.createElement(App));
+root.render(<App />);
