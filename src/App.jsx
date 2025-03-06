3cbe720a2774d60a6117509553e1b76d52ffcf20
PattyDor <pado@ok.dk>
2025-03-06 14:04:25 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
 function App() {
-  return <h1>🚀 React is Working!</h1>;
+  return (
+    <div>
+      <h1 style={{ color: "red" }}>🚀 React is Working!</h1>
+      <p>If you see this, React is rendering properly.</p>
+    </div>
+  );
 }
 
 export default App;
